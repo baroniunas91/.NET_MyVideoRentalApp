7dd2485e66aca29c03632bb04093e67aaece6be4
Kęstutis Keršis <kkersis@alnasoft.lt>
2021-03-16 12:48:31 +0200

    20-created Movie and Genre models and added them to db

--- VideoRent/Data/Migrations/20210316104326_MoviesAndGenres.Designer.cs
+++ VideoRent/Data/Migrations/20210316104326_MoviesAndGenres.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoRent.Data;
 
 namespace VideoRent.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210316104326_MoviesAndGenres")]
+    partial class MoviesAndGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VideoRent/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoRent.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's release date.")]
+        [Display(Name = "Release Date")]
+        public DateTime? RealeaseDate { get; set; }
+
+        [Required(ErrorMessage = "Please select movie's genre.")]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public int NumberInStock { get; set; } = 0;
+
+        [Display(Name = "Number Available")]
+        public int NumberAvailable { get; set; } = 0;
+
+        public Genre Genre { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+    }
+}
