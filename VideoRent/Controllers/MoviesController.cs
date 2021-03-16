5cdf1f92fd7d6af7d90ff4eb24eed5a7b1ad2c20
Kęstutis Keršis <kkersis@alnasoft.lt>
2021-03-16 13:08:43 +0200

    20-modified movies controller and view to list movies

--- VideoRent/Controllers/MoviesController.cs
+++ VideoRent/Controllers/MoviesController.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VideoRent.Data;
 
 namespace VideoRent.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var movies = _context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+
+            return View(movies);
         }
     }
 }
