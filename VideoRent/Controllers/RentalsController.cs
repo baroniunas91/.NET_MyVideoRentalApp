875d96b6bff1b2b7805e1cab9a996f6dfd9b2dcb
Edgaras Baroniūnas <ebaroniunas@alnasoft.lt>
2021-03-17 11:14:15 +0200

    25 - add Rentals tab

---
+++ VideoRent/Controllers/RentalsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoRent.Data;
+
+namespace VideoRent.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RentalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
